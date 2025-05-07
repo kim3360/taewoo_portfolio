fe196f192a78b2f012aa16d49ac6f7424af2fe43
KimTaeWoo <70637743+kim3360@users.noreply.github.com>
2025-05-07 15:56:10 +0900

    feat: About_me & Project 컴포넌트 분리 (#3)

---
+++ src/Project.tsx
@@ -0,0 +1,21 @@
+// import React from "react";
+
+
+
+export const Project = () => {
+
+
+
+    return (
+        <>
+
+            {/* Transition Gradient */}
+            <div className="h-32 bg-gradient-to-b from-black/50 to-[#0f172a]"></div>
+
+            <div className="h-screen bg-[#0f172a] text-white"></div>
+
+        </>
+    );
+};
+
+
