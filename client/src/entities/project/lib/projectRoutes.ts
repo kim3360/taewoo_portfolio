a5851f83d9c4b7912c5a2b0b8e05f1200d156eff
kimtaewoo <70637743+kim3360@users.noreply.github.com>
2026-06-14 06:28:23 +0900

    fix : 다솜 프로젝트 트러블 슈팅 수정

--- client/src/entities/project/lib/projectRoutes.ts
+++ client/src/entities/project/lib/projectRoutes.ts
@@ -1,13 +1,13 @@
-import { projects } from "../model/projects";
+import { projects } from "../model/projects"
 
-export type { ProjectDetails, ProjectFeature, ProjectBase } from "../model/projectTypes";
+export type { ProjectDetails, ProjectFeature, ProjectBase } from "../model/projectTypes"
 
-export type Project = (typeof projects)[number];
+export type Project = (typeof projects)[number]
 
 export function getProjectPath(slug: string) {
-  return `/projects/${slug}`;
+  return `/projects/${slug}`
 }
 
 export function findProjectBySlug(slug: string): Project | undefined {
-  return projects.find((project) => project.slug === slug);
+  return projects.find((project) => project.slug === slug)
 }
